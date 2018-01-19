56dd2bd96bb9b9a82e5b464743e1f2c0921c1201
Mason X <masonx.01@gmail.com>
2018-01-18 22:45:57 -0500

    Fix newlines
    This is actually a bit of a dilemma...
    
    IRC protocol uses `\r\n`. But, macOS uses `\n` to delimit lines... What should we use?
    
    Well, because we have more Windows machines than macOS machines, the verdict is in... use `\r\n`! Let macOS deal with its troubles by itself >:(

--- public/javascripts/ohnx-extras.js
+++ public/javascripts/ohnx-extras.js
@@ -3,7 +3,7 @@
     var modifyCopy = e => {
         var data = window.getSelection().toString();
         console.log(data);
-        data = data.replace(/,?([0-9:]+)\t(.*)\t(.*)\n?/g, "[$1] <$2> $3\n");
+        data = data.replace(/,?([0-9:]+)\t(.*)\t(.*)\n?/g, "[$1] <$2> $3\r\n");
         e.clipboardData.setData('text/plain', data);
         e.preventDefault();
     };
